896ef71afb17e065631a2582644f60ae5463c516
Gabriel Petry <gabriel.petry@kinghost.com.br>
2021-06-02 10:55:21 -0300

    group users by company

--- packages/server/routes/users.js
+++ packages/server/routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
-
+const api = require('../services/jsonPlaceholder')
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.send({ ok: true })
+router.get('/', async (req, res) => {
+  const users = await api.get('/users').then(res => res.data)
+
+  const usersReduce = users.reduce((accumulator, user) => {
+    accumulator[user.company.name] = accumulator[user.company.name] || []
+    accumulator[user.company.name].push(user)
+    return accumulator
+  }, Object.create(null))
+  
+  res.send({ ok: true, usersReduce })
 })
 
 module.exports = router
